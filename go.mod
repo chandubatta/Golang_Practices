ff1044f6b3662089d4f88d9dde3a5838cb6c5845
chandubatta <battachandu996699@gmail.com>
2026-01-02 16:44:31 +0530

    simple package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chandubatta/Golang_Practices
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"myproject/utils"
+
+	"github.com/chandubatta/Golang_Practices/utils"
+)
+
+func main() {
+	a := 20
+	b := 30
+	result := utils.Add(a, b)
+	fmt.Println("Result is :", result)
+}
